745401d204d72cc241003f8a71c8928f2120fcb7
Aman Sharma <iak.sharma0004@gmail.com>
2022-10-18 16:22:05 +0530

    gamaplady enhancement

--- constants/event-constants.ts
+++ constants/event-constants.ts
@@ -2,7 +2,9 @@ export const IN_EVENT = {
   JOIN_ROOM: 'join_room',
   BROADCAST_MESSAGE: 'client_to_server_message',
   PLAYED: 'played',
-  START_GAME: 'start_game'
+  START_GAME: 'start_game',
+  GAME_WON: 'game_won',
+  ANOTHER_GAME: 'another_game'
 }
 
 export const OUT_EVENT = {
@@ -12,5 +14,7 @@ export const OUT_EVENT = {
   OPPONENT_JOINED: 'opponent_joined',
   OPPONENT_LEFT: 'opponent_left',
   START_GAME: 'start_game',
-  NEXT_TURN: 'next_turn'
+  NEXT_TURN: 'next_turn',
+  OPPONENT_WON: 'opponent_won',
+  ANOTHER_GAME: 'another_game'
 }
